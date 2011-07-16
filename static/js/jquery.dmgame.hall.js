f74a7dff567d75b7ce7fc1340cd3e3685644ee9e
Mic <mic-dm@mail.ru>
2011-07-16 22:39:07 +0700

    new: базовое взаимодействие с игровым залом

--- static/js/jquery.dmgame.hall.js
+++ static/js/jquery.dmgame.hall.js
@@ -6,8 +6,19 @@
 (function($) {
 
 	$.dmgame.hall = function() {
+		// После аутентификации шлем запрос на вход в зал:
 		$.dmgame.dispatcher.bind('ok.auth', function() {
-			
+			$.dmgame.dispatcher.trigger_outcoming(['hall', 'enter']);
+		});
+		
+		// После входа в зал встаем в очередь игроков:
+		$.dmgame.dispatcher.bind_to_incoming(['hall', 'welcome'], function() {
+			$.dmgame.dispatcher.trigger_outcoming(['hall', 'play']);
+		});
+		
+		// Принимаем приглашение вступить в группу:
+		$.dmgame.dispatcher.bind_to_incoming(['hall', 'party_invite'], function() {
+			$.dmgame.dispatcher.trigger_outcoming(['hall', 'accept_invite']);
 		});
 	};
 
